c9ffba4452c67d09b58c489d7ad5abaed5c4d6a6
Nikola Mitrovic <nmitrovic470@gmail.com>
2020-10-31 22:02:05 +0100

    UUID generator

--- server.js
+++ server.js
@@ -2,17 +2,28 @@ const express = require('express')
 const app = express()
 const server = require('http').Server(app)
 const io = require('socket.io')(server)
-const { v4: uuidV4 } = require('uuid')
+
+// Function for generating universally unique identifier
+function create_UUID(){
+    var dt = new Date().getTime();
+    var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = (dt + Math.random()*16)%16 | 0;
+        dt = Math.floor(dt/16);
+        return (c=='x' ? r :(r&0x3|0x8)).toString(16);
+    });
+    return uuid;
+}
 
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-    res.redirect(`/${uuidV4}`)
+    res.redirect(`/${create_UUID()}`)
 })
 
 app.get('/:room', (req, res) => {
     res.render('room', { roomId: req.params.room })
 })
+
 
 server.listen(3000)
